61b04b0122e2ee7ac39d003a0097812fb07949c5
SANAT-01 <sanattudu28@gmail.com>
2025-09-01 22:13:38 +0530

    added joi validation

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,17 +1,18 @@
 import express from 'express'; 
 import { createUser, deleteUser, getUserById, getUsers, updateUser } from '../controllers/userController.js';
+import validateUser from '../middlewares/userValidate.js';
 
 const router = express.Router();
 
 // GET /users
-router.get('/user', getUsers);
+router.get('/user', validateUser, getUsers);
 router.get('/user/:id', getUserById);
 
 // POST /users
-router.post('/user', createUser);
+router.post('/user', validateUser, createUser);
 
 // PUT /users/:id
-router.put('/user/:id', updateUser);
+router.put('/user/:id', validateUser, updateUser);
 
 // DELETE /users/:id
 router.delete('/user/:id', deleteUser);
