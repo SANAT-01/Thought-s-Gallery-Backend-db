07b2a12a7c7f3288dfab2e901a6a61f1476da04c
SANAT-01 <sanattudu28@gmail.com>
2025-09-02 21:48:45 +0530

    added ssl for live database

--- src/config/db.js
+++ src/config/db.js
@@ -12,6 +12,7 @@ const pool = new Pool({
     max: 20, // max number of clients in the pool
     idleTimeoutMillis: 30000, // close idle clients after 30 seconds
     connectionTimeoutMillis: 2000, // return an error after 2 seconds if connection could not be established
+    ssl: {rejectUnauthorized: false}, // Required for Render
 });
 
 pool.on("connect", () => {
